4fe954e770880518ae7844242f7d40fe99d6a90c
PanduSama <pandurosama@gmail.com>
2025-08-12 19:12:14 -0300

    feat: Agrego comando list para mostrar notas

--- src/main.sh
+++ src/main.sh
@@ -3,3 +3,10 @@ if [ "$1" = "add" ]; then
  echo "$2" >> notes.txt
  echo "Nota añadida: $2"
 fi
+if [ "$1" = "list" ]; then
+ if [ -f notes.txt ]; then
+  cat notes.txt
+ else
+  echo "No hay notas disponibles"
+ fi
+fi
